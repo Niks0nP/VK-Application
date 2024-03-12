29cf853c970c9e19f62bcbf0ab103b3811d38fd7
Niks0nP <nikita.niko40@gmail.com>
2024-03-12 21:02:29 +0300

    add button pager change

--- app/src/main/java/nikita/app/vkapplication/data/network/ApiService.kt
+++ app/src/main/java/nikita/app/vkapplication/data/network/ApiService.kt
@@ -2,7 +2,6 @@ package nikita.app.vkapplication.data.network
 
 import nikita.app.vkapplication.data.model.ParseInfo
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -10,5 +9,4 @@ interface ApiService {
     @GET("products?&limit=20")
     suspend fun getProducts(@Query("skip") skipElements: Int) : ParseInfo
 
-//@Path("skipElements") skipElements: Int
 }
